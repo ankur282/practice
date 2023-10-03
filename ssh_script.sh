1240fa84a823f41cedfe5e858770a51ad92ebda4
ankur <ankurmatariya@gamil.com>
2023-10-03 17:09:07 +0530

    new modification with ssh

--- ssh_script.sh
+++ ssh_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set the SSH username, hostname, and password
-USERNAME="einfochip"
-HOSTNAME="10.126.20.172"
-PASSWORD="qwe1234"
+USERNAME="enter user name"
+HOSTNAME="enter ip "
+PASSWORD="enter password"
 
 # SSH command using sshpass
 sshpass -p "$PASSWORD" ssh "$USERNAME@$HOSTNAME"
